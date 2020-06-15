43cbc5c3d52bcce1f8628927f4fc0d853b18a30d
Huo <33674202@qq.com>
2020-06-15 14:06:49 +0800

    mybatis plus Generator

--- src/main/java/MpGenerator.java
+++ src/main/java/MpGenerator.java
@@ -1,11 +1,11 @@
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
-import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.GlobalConfig;
-import com.baomidou.mybatisplus.generator.config.PackageConfig;
-import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 /**
  * <p>
@@ -14,45 +14,32 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
  */
 public class MpGenerator {
 
-    // 输出路径
-    final static String outPath = "C:\\Users\\Huo\\Desktop\\新建文件夹";
+    /**
+     * 输出路径
+      */
+
+    final static String outPath = "C:\\Users\\Huohuo\\Desktop\\新建文件夹";
     // 数据库地址
     final static String url =
-        "jdbc:mysql://218.60.3.126:23306/auth_huinong?characterEncoding=UTF-8&useUnicode=true&useSSL=false";
+        "jdbc:mysql://120.201.137.165:6021/landsubsidy?characterEncoding=UTF-8&useUnicode=true&useSSL=false";
     // 用户名
-    final static String username = "root";
+    final static String username = "admin_gx01";
     // 密码
-    final static String password = "Unicom@2019";
+    final static String password = "Admin_gx01";
     // 作者
     final static String author = "huozhenbin";
     // 父级包名
-    final static String parentPackage = "org.huinong.wechatauth.modules.wechatauth";
+    final static String parentPackage = "org.jeecg.modules.basisorg";
     // 表名数组
-    final static String[] includeTables = {};
-    final static String[] excludeTables = {""};
+//    final static String[] includeTables = {"auth_user_role","farmer_base_info","auth_user_info","auth_role"};
+    final static String[] includeTables = {"risk_farmer_base_info"};
+
+    final static String[] excludeTables = {};
     // 表名前缀数组
     final static String[] tablePrefix = {};
 
     public static void main(String[] args) {
-        AutoGenerator mpg = new AutoGenerator();
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir(outPath);
-        gc.setFileOverride(true);
-        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
-        gc.setEnableCache(false);// XML二级缓存
-        gc.setBaseResultMap(false);// XML ResultMap
-        gc.setBaseColumnList(false);// XML columList
-        gc.setAuthor(author);// 作者
-
-        // 自定义文件命名，注意%s 会自动填充表实体属性
-        gc.setControllerName("%sController");
-        gc.setServiceName("%sService");
-        gc.setServiceImplName("%sServiceImpl");
-        gc.setMapperName("%sMapper");
-        gc.setXmlName("%sMapper");
-        mpg.setGlobalConfig(gc);
-
+        AutoGenerator autoGenerator = new AutoGenerator();
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setDbType(DbType.MYSQL);
@@ -60,7 +47,32 @@ public class MpGenerator {
         dsc.setUsername(username);
         dsc.setPassword(password);
         dsc.setUrl(url);
-        mpg.setDataSource(dsc);
+        autoGenerator.setDataSource(dsc);
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(outPath);
+        gc.setFileOverride(true);
+        // ActiveRecord特性
+        gc.setActiveRecord(false);
+        // XML二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(false);
+        // XML columList
+        gc.setBaseColumnList(true);
+        gc.setAuthor(author);// 作者
+        gc.setSwagger2(true);// Swagger2
+        // 日期类型
+        gc.setDateType(DateType.ONLY_DATE);
+        gc.setIdType(IdType.ID_WORKER_STR);
+
+        // 自定义文件命名，注意%s 会自动填充表实体属性
+        gc.setControllerName("%sController");
+        gc.setServiceName("I%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        autoGenerator.setGlobalConfig(gc);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
@@ -68,17 +80,18 @@ public class MpGenerator {
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
         if (includeTables.length > 0) {
             strategy.setInclude(includeTables);// 需要生成的表
+        } else {
+            if (excludeTables.length > 0) {
+                strategy.setExclude(excludeTables);// 需要排除的表
+            }
         }
-        if (excludeTables.length > 0) {
-            strategy.setExclude(excludeTables);// 需要排除的表
-        }
+        strategy.setRestControllerStyle(true);
         strategy.setEntityLombokModel(true);
         strategy.setSuperServiceClass(null);
         strategy.setSuperServiceImplClass(null);
         strategy.setSuperMapperClass(null);
 
-        mpg.setStrategy(strategy);
-
+        autoGenerator.setStrategy(strategy);
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setParent(parentPackage);
@@ -88,7 +101,7 @@ public class MpGenerator {
         pc.setMapper("mapper");
         pc.setEntity("entity");
         pc.setXml("mapper.xml");
-        mpg.setPackageInfo(pc);
+        autoGenerator.setPackageInfo(pc);
         // 自定义属性注入 没有这个不报错 但是生成失败
         InjectionConfig injectionConfig = new InjectionConfig() {
             // 自定义属性注入:abc
@@ -101,10 +114,10 @@ public class MpGenerator {
             }
         };
         // 配置自定义属性注入
-        mpg.setCfg(injectionConfig);
-
+        autoGenerator.setCfg(injectionConfig);
+        autoGenerator.setTemplateEngine(new FreemarkerTemplateEngine());
         // 执行生成
-        mpg.execute();
+        autoGenerator.execute();
     }
 
 }
